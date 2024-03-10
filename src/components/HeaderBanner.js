14bbc63e3c3861f5f68010a847cf1db27c51d952
emarketingcontacto <emarketing.contacto@gmail.com>
2024-03-10 14:48:00 -0600

    changes on name

--- src/components/HeaderBanner.js
+++ src/components/HeaderBanner.js
@@ -5,8 +5,10 @@ import "./HeaderBanner.css";
 
 const HeaderBanner = () => {
   return (
-	  <div className='header-banner' style={{backgroundImage:`url(${Background})`}}>
-		  
+	  <div className='header-banner' style={{ backgroundImage: `url(${Background})` }}>
+		  <div className="d-flex justify-content-start align-items-center w-100">
+			  <h1>Centro de Atención Veterinaria Las Torres</h1>
+		  </div>
 	  </div>
   )
 }
